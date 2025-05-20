d4f06f1f47e5c0b59ddec3e598f954875fc6a597
Didier <didiertoken@mail.com>
2025-05-20 15:13:05 +1000

    Update Footer to use new SocialButtons component

--- apps/web/src/components/Footer.tsx
+++ apps/web/src/components/Footer.tsx
@@ -1,10 +1,10 @@
-import { IconTwitterX } from "./Icons";
+import { SocialButtons } from "./SocialButtons";
 
 export function Footer() {
   return (
     <footer className="w-full flex justify-center mt-8 mb-4">
-      <div className="text-white opacity-60 hover:opacity-100 transition-opacity duration-300 transform scale-150">
-        <IconTwitterX size={96} />
+      <div className="transition-opacity duration-300">
+        <SocialButtons />
       </div>
     </footer>
   );
